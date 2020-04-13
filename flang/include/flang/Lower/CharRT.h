1cd4b9d1bdda84ccbbd9c989f7ce459e861bae65
Eric Schweitz <eschweitz@nvidia.com>
2020-04-13 13:36:14 -0700

    add plumbing to connect up character comparison; add test

--- flang/include/flang/Lower/CharRT.h
+++ flang/include/flang/Lower/CharRT.h
@@ -9,8 +9,7 @@
 #ifndef FORTRAN_LOWER_CHARRT_H
 #define FORTRAN_LOWER_CHARRT_H
 
-#include "flang/Common/Fortran.h"
-#include "mlir/IR/Value.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
 
 namespace Fortran {
 namespace lower {
@@ -19,17 +18,17 @@ class AbstractConverter;
 /// Generate call to a character comparison for two ssa-values of type
 /// `boxchar`.
 mlir::Value genBoxCharCompare(AbstractConverter &converter, mlir::Location loc,
-                              common::RelationalOperator cmp,
-                              mlir::Value lhsBuff, mlir::Value rhsBuff);
+                              mlir::CmpIPredicate cmp, mlir::Value lhs,
+                              mlir::Value rhs);
 
 /// Generate call to a character comparison op for two unboxed variables. There
 /// are 4 arguments, 2 for the lhs and 2 for the rhs. Each CHARACTER must pass a
 /// reference to its buffer (`ref<char<K>>`) and its LEN type parameter (some
 /// integral type).
 mlir::Value genRawCharCompare(AbstractConverter &converter, mlir::Location loc,
-                              common::RelationalOperator cmp,
-                              mlir::Value lhsBuff, mlir::Value lhsLen,
-                              mlir::Value rhsBuff, mlir::Value rhsLen);
+                              mlir::CmpIPredicate cmp, mlir::Value lhsBuff,
+                              mlir::Value lhsLen, mlir::Value rhsBuff,
+                              mlir::Value rhsLen);
 
 } // namespace lower
 } // namespace Fortran
