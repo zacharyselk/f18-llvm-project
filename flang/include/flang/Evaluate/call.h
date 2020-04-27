c9aa23ab10d0cd71b62e5bff8881171c48942545
vdonaldson <37090318+vdonaldson@users.noreply.github.com>
2020-04-27 11:41:51 -0700

    Implement subroutine calls with alternate returns. (#26)
    Implement subroutine calls with alternate returns.

--- flang/include/flang/Evaluate/call.h
+++ flang/include/flang/Evaluate/call.h
@@ -190,8 +190,9 @@ struct ProcedureDesignator {
 class ProcedureRef {
 public:
   CLASS_BOILERPLATE(ProcedureRef)
-  ProcedureRef(ProcedureDesignator &&p, ActualArguments &&a)
-      : proc_{std::move(p)}, arguments_(std::move(a)) {}
+  ProcedureRef(ProcedureDesignator &&p, ActualArguments &&a, bool alt = false)
+      : proc_{std::move(p)}, arguments_{std::move(a)}, hasAlternateReturns_{
+                                                           alt} {}
   ~ProcedureRef();
 
   ProcedureDesignator &proc() { return proc_; }
@@ -202,12 +203,14 @@ public:
   std::optional<Expr<SubscriptInteger>> LEN() const;
   int Rank() const;
   bool IsElemental() const { return proc_.IsElemental(); }
+  bool HasAlternateReturns() const { return hasAlternateReturns_; }
   bool operator==(const ProcedureRef &) const;
   llvm::raw_ostream &AsFortran(llvm::raw_ostream &) const;
 
 protected:
   ProcedureDesignator proc_;
   ActualArguments arguments_;
+  bool hasAlternateReturns_;
 };
 
 template <typename A> class FunctionRef : public ProcedureRef {
