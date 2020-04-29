56ff94daf43e3962735332c36035ea7f87b5eb7e
Eric <eschweitz@nvidia.com>
2020-04-29 16:42:15 -0700

    revert last patch

--- flang/lib/Lower/ConvertExpr.cpp
+++ flang/lib/Lower/ConvertExpr.cpp
@@ -576,11 +576,7 @@ private:
       }
       llvm_unreachable("array of complex unhandled");
     } else if constexpr (TC == Fortran::lower::CharacterCat) {
-      // Wrap this in a box. Return a pair?
-      auto addr = genCharLit<KIND>(con.GetScalarValue().value(), con.LEN());
-      auto len =
-        builder.createIntegerConstant(builder.getIndexType(), con.LEN());
-      return builder.createEmboxChar(addr, len);
+      return genCharLit<KIND>(con.GetScalarValue().value(), con.LEN());
     } else {
       llvm_unreachable("unhandled constant");
     }
