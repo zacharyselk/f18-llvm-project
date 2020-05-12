662048ffb4ab8dc3a91d03a73a50e508f1a86ce8
Eric <eschweitz@nvidia.com>
2020-05-12 08:08:23 -0700

    revert PR #61

--- flang/lib/Optimizer/Dialect/FIRType.cpp
+++ flang/lib/Optimizer/Dialect/FIRType.cpp
@@ -915,7 +915,7 @@ CplxType fir::CplxType::get(mlir::MLIRContext *ctxt, KindTy kind) {
   return Base::get(ctxt, FIR_COMPLEX, kind);
 }
 
-mlir::Type fir::CplxType::getEleTy() const {
+mlir::Type fir::CplxType::getElementType() const {
   return fir::RealType::get(getContext(), getFKind());
 }
 
