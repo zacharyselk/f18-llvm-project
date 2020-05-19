01ca055ba1e9b8b1b4f436d0cb378b4a2d645a20
Eric Schweitz <eschweitz@nvidia.com>
2020-05-19 07:38:13 -0700

    Another crash apparently related to issue #74. Add check for empty list.

--- flang/lib/Lower/Bridge.cpp
+++ flang/lib/Lower/Bridge.cpp
@@ -1668,7 +1668,8 @@ private:
     if (unstructuredContext) {
       // When transitioning from unstructured to structured code,
       // the structured code might be a target that starts a new block.
-      maybeStartBlock(eval.isConstruct() && eval.lowerAsStructured()
+      maybeStartBlock(eval.isConstruct() && eval.lowerAsStructured() &&
+                              !eval.evaluationList->empty()
                           ? eval.evaluationList->front().block
                           : eval.block);
     }
