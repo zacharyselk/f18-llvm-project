82a41cb4613a1555c08e5e8f73d649b36d76ae99
zachary-selk <zachary.selk@ibm.com>
2020-05-26 13:59:29 -0600

    Adjusting properly now

--- flang/lib/Optimizer/Transforms/RewriteLoop.cpp
+++ flang/lib/Optimizer/Transforms/RewriteLoop.cpp
@@ -44,8 +44,8 @@ public:
     auto step = loop.step();
     assert(low && high && step);
     // ForOp has different bounds semantics. Adjust upper bound.
-    auto one = rewriter.create<mlir::ConstantIndexOp>(loc, 1);
-    auto adjustUp = rewriter.create<mlir::AddIOp>(loc, high, one);
+    auto diff = rewriter.create<mlir::SubIOp>(loc, high, low);
+    auto adjustUp = rewriter.create<mlir::AddIOp>(loc, diff, step);
     auto f = rewriter.create<mlir::scf::ForOp>(loc, low, adjustUp, step);
     f.region().getBlocks().clear();
     rewriter.inlineRegionBefore(loop.region(), f.region(), f.region().end());
