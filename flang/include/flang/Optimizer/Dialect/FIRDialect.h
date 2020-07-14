aff88782ecb51c389ef6f72851389273b4d5938d
Sourabh Singh Tomar <SourabhSingh.Tomar@amd.com>
2020-07-14 20:08:23 +0530

    [flang][OpenMP] downstream merge after 3d0b76022df
    This merges remaining parts of:
    https://reviews.llvm.org/D83659

--- flang/include/flang/Optimizer/Dialect/FIRDialect.h
+++ flang/include/flang/Optimizer/Dialect/FIRDialect.h
@@ -47,7 +47,6 @@ inline void registerFIR() {
     mlir::registerDialect<mlir::StandardOpsDialect>();
     mlir::registerDialect<mlir::vector::VectorDialect>();
     mlir::registerDialect<FIROpsDialect>();
-    mlir::registerDialect<mlir::omp::OpenMPDialect>();
     return true;
   }();
 }
